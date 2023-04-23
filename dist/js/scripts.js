1f660b3bdb3ef7bb31cb41debabd344891586b30
Nathan <40289071+nathan-starkey@users.noreply.github.com>
2023-04-23 17:26:09 +1200

    Implement loading file data

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,11 +1,15 @@
-renderEntryList(["Option 1", "Option 2", "Option 3"]);
+let handle = null;
+let content = null;
+
+// renderEntryList(["Option 1", "Option 2", "Option 3"]);
 
 btnFileAction.addEventListener("click", openFile);
 
 async function openFile() {
   btnFileAction.disabled = true;
 
-  let handle;
+  let handle_;
+  let content_;
 
   let options = {
     types: [{
@@ -16,22 +20,59 @@ async function openFile() {
 
   try {
     // Request a file handle from the user
-    [handle] = await showOpenFilePicker(options);
+    [handle_] = await showOpenFilePicker(options);
   } catch (e) {
     // Perhaps the user cancelled this action
     btnFileAction.disabled = false;
     return;
   }
 
+  try {
+    let blob = await handle_.getFile();
+    let text = await blob.text();
+    content_ = JSON.parse(text);
+  } catch (e) {
+    // Something went wrong loading the file
+    alert("Problem encountered while trying to read the file:\n\n" + e);
+    btnFileAction.disabled = false;
+    return;
+  }
+
+  handle = handle_;
+  content = content_;
+
   // Update our view with the file's data
-  lblFileLabel.innerText = handle.name;
+  lblFileLabel.innerText = handle_.name;
   btnFileAction.innerText = "Save File";
   btnFileAction.removeEventListener("click", openFile);
+  btnFileAction.addEventListener("click", saveFile);
+  renderEntryList([
+    ...content.creatures.map(o => o.id),
+    ...content.tiles.map(o => o.id),
+    ...content.worlds.map(o => o.id)
+  ]);
+}
+
+async function saveFile() {
+  btnFileAction.disabled = true;
+
+  try {
+    let writable = await handle.createWritable();
+    writable.write(JSON.stringify(content));
+    writable.close();
+  } catch (e) {
+    alert("Problem encountered while trying to write to the file:\n\n" + e);
+    btnFileAction.disabled = false;
+    return
+  }
 }
 
 function renderEntryList(items) {
   // Clear the current list
   ctrEntryList.innerHTML = "";
+
+  // Sort the list in alphabetic order
+  items.sort((a, b) => a < b ? -1 : 1);
 
   for (let item of items) {
     // Clone the template list item
