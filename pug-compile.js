671ccb6c3b13eceaedb90ad0184db15df7f970d3
Nathan <40289071+nathan-starkey@users.noreply.github.com>
2023-04-23 21:48:44 +1200

    Removed files from .gitignore and prettified index.html for source control

--- pug-compile.js
+++ pug-compile.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
 const pug = require("pug");
+const toDiffableHtml = require("diffable-html");
 
-fs.writeFileSync("dist/index.html", pug.renderFile("src/index.pug"));
+const notice = "<!-- Auto-generated: DO NOT EDIT -->\n";
+
+fs.writeFileSync("dist/index.html", notice + toDiffableHtml( pug.renderFile("src/index.pug")).trim());
