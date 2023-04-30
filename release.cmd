647df4c8020aee8389ee91eee65117dffce49a2b
Nathan <40289071+nathan-starkey@users.noreply.github.com>
2023-05-01 11:11:53 +1200

    Fix release script using wrong archive format

--- release.cmd
+++ release.cmd
@@ -3,7 +3,9 @@ call build
 if exist release\ rmdir /s /q release
 mkdir release
 mkdir release\cloak-data-editor
-xcopy build\* release\cloak-data-editor\
-tar -cf release\cloak-data-editor.zip release\cloak-data-editor
+xcopy /S build\* release\cloak-data-editor\
+pushd release
+tar -a -cf cloak-data-editor.zip cloak-data-editor
+popd
 copy src\scripts\update.cmd release\update.cmd
 rmdir /s /q release\cloak-data-editor\
