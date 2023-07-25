bf02e6b1197aab2984735e49a67d7d31e087b0bd
Nathan <40289071+nathan-starkey@users.noreply.github.com>
2023-07-25 22:50:02 +1200

    Add Bootstrap extended with Sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./assets/styles/index.scss";
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
