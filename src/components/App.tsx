3e5f52c80de277b8367c75c29aa5eeb039515d98
Nathan <40289071+nathan-starkey@users.noreply.github.com>
2023-07-26 03:25:01 +1200

    Add cloak-data-utils module to enable access to the content folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import ProjectStatus from "./ProjectStatus";
 import Sidebar from "./Sidebar";
+import { readContentFolder } from "cloak-data-utils/ContentFolder";
 
 export default function App() {
   let [name, setName] = useState("");
   let [isBusy, setIsBusy] = useState(false);
   let [isOpen, setIsOpen] = useState(false);
   let [isSaved, setIsSaved] = useState(false);
-
+  
   function handleOpen() {
+    setIsBusy(true);
+    readContentFolder((folder, file, content, images) => {
+      setName(folder.name);
+      setIsOpen(true);
+      setIsBusy(false);
+      setIsSaved(true);
+    }, error => {
+      setIsBusy(false);
+      alert(error);
+    });
   }
 
   function handleSave() {
