e303dcc586d8b08c3d1ff7bd775f67072c58126a
Nathan Starkey <40289071+nathan-starkey@users.noreply.github.com>
2024-05-27 02:46:55 +1200

    Change API to require explicit property accessors

--- src/ts/cloak/forms.ts
+++ src/ts/cloak/forms.ts
@@ -4,31 +4,18 @@ import {
   ControlBoolean as bool
 } from "../api/controls";
 
-import { create_form } from "../api/forms";
+import {
+  create_form,
+  create_property_accessor as prop
+} from "../api/forms";
+
 import * as data from "./types/data";
 
-export function create_image_form(image: data.Image) {
-  return create_form([
-    ["path", "Path", str]
-    // TODO: ...
-  ], image);
-}
-
-export function create_sprite_form(images: data.Image[], sprite: data.Sprite) {
-  return create_form([
-    ["x", "X", num],
-    ["y", "Y", num],
-    ["height", "Height", num],
-    ["width", "Width", num]
-    // TODO: ...
-  ], sprite);
-}
-
 export function create_tile_form(tile: data.Tile) {
-  return create_form([
-    ["name", "Name", str],
-    ["description", "Description", str],
-    ["is_opaque", "Is Opaque", bool],
-    ["is_solid", "Is Solid", bool]
-  ], tile);
+  return create_form(tile, [
+    ["Name", str, prop("name")],
+    ["Description", str, prop("description")],
+    ["Is Opaque", bool, prop("is_opaque")],
+    ["Is Solid", bool, prop("is_solid")]
+  ]);
 }
