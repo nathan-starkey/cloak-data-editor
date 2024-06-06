7a0b02f7ebd2cf843c3c88d5934d3677c5ad2db5
Nathan Starkey <40289071+nathan-starkey@users.noreply.github.com>
2024-06-06 15:38:45 +1200

    Fix a missing variable initialization

--- src/ts/utils/object-property-proxy.ts
+++ src/ts/utils/object-property-proxy.ts
@@ -2,7 +2,7 @@ import { Proxy, Observer } from "../types";
 
 /** Create a proxy for getting and setting an object property. */
 export function use_object_property<T, K extends keyof T>(object: T, property: K): Proxy<T[K]> & Observer<T[K]> {
-  let observers: ((value: T[K]) => void)[];
+  let observers: ((value: T[K]) => void)[] = [];
 
   let proxy = {
     get() {
